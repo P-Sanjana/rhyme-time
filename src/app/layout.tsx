447040b49b37c2caf63df278e524a3cfa89f65e7
P-Sanjana <podduturisanjana@gmail.com>
2025-10-17 00:54:34 -0400

    added counter before game start ✨

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,6 +1,7 @@
 import type { Metadata } from 'next';
 import { Geist, Geist_Mono, Poppins } from 'next/font/google';
 import './globals.css';
+import Header from './components/header/Header';
 
 const geistSans = Geist({
   variable: '--font-geist-sans',
@@ -33,6 +34,7 @@ export default function RootLayout({
       <body
         className={`${poppins.className} ${geistSans.variable} ${geistMono.variable} antialiased`}
       >
+          <Header />
           {children}
       </body>
     </html>
