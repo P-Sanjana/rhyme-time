5cb42fbc067f187e0ebad1ba204387ac0c14521c
P-Sanjana <podduturisanjana@gmail.com>
2025-11-12 19:48:30 -0500

    added 3d scenes in home 💫

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,16 +1,39 @@
 'use client';
-import { useState } from "react";
-import UnicornSceneComponent from "./components/unicorn-scene-home/UnicornSceneHome";
-import GameSetupModal from "./components/game-setup-modal/GameSetupModal";
+
+import dynamic from 'next/dynamic';
+import LightRays from '@/components/shadcn/LightRays';
+import Header from '@/components/header/Header';
+import SplashCursor from '@/components/shadcn/SplashCursor';
 
 const Home = () => {
-  const [difficulty, setDifficulty] = useState('Easy');
+  const CubeScene = dynamic(
+    () => import('../components/3d-cube-scene/Cube-Scene'),
+    { ssr: false }
+  );
   return (
-    <div className="bg-black">
-      {/* <Header /> */}
-      {/* <UnicornSceneComponent /> */}
-      {/* <GameSetupModal /> */}
+    <div className='relative bg-black h-screen overflow-hidden'>
+      <SplashCursor />
+      <div className='absolute inset-0 z-0'>
+        <LightRays
+          raysOrigin='top-center'
+          raysColor='#ef9967'
+          raysSpeed={1.5}
+          lightSpread={0.8}
+          rayLength={1.2}
+          followMouse={true}
+          mouseInfluence={0.1}
+          noiseAmount={0.1}
+          distortion={0.05}
+          className='custom-rays'
+        />
+      </div>
+      <div className='relative z-20'>
+        <Header />
+      </div>
+      <div className='absolute inset-0 z-10'>
+        <CubeScene />
+      </div>
     </div>
   );
-}
- export default Home
+};
+export default Home;
