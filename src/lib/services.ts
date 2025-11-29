c7b0753e97fdf765cb1cb7bc21c3262bbfe314cf
P-Sanjana <podduturisanjana@gmail.com>
2025-11-29 12:35:33 -0500

    random word API call 🗃️

---
+++ src/lib/services.ts
@@ -0,0 +1,10 @@
+const BASE_URL = 'https://rhyme-time-backend.onrender.com';
+export const getRandomWord = async (difficulty: string) => {
+  try {
+    const res = await fetch(`${BASE_URL}/randomword?difficulty=${difficulty}`);
+    const data = await res.json();
+    return data.word;
+  } catch (error) {
+    console.error('Error fetching data', error);
+  }
+};
